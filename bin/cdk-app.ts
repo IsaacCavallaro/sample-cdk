8518b28a7b392c9031dbfaf619bc73e3f5eddd57
isaaccavallaro <isaaccavallaro@gmail.com>
2022-12-04 20:44:43 +1000

    Renaming stack, succesfully deployed to user

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -4,7 +4,7 @@ import {CdkAppStack} from '../lib/cdk-app-stack';
 
 const app = new cdk.App();
 
-new CdkAppStack(app, 'CdkAppStack', {
+new CdkAppStack(app, 'cdk-stack-dev', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
