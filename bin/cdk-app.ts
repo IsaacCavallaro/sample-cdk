81d9c6f9743c1c49275d255ae62092d477764fee
isaaccavallaro <isaaccavallaro@gmail.com>
2022-12-05 18:29:27 +1000

    Adding second stack - dev and prod

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -5,6 +5,15 @@ import {CdkAppStack} from '../lib/cdk-app-stack';
 const app = new cdk.App();
 
 new CdkAppStack(app, 'cdk-stack-dev', {
+  stackName: 'cdk-stack-dev',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+new CdkAppStack(app, 'cdk-stack-prod', {
+  stackName: 'cdk-stack-prod',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
